068a41c8cad518e3864f5e65d3891d13b7d8008c
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-30 14:51:18 +0100

    Question 3 terminer:Écrivez une fonction appelée convertArrayToObject qui peut convertir le tableau en un objet de structure.

--- Exo3.js
+++ Exo3.js
@@ -8,3 +8,16 @@ for (const {name, capital, population, languages} of countries) {
 const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
 let [nom, skills, scores] = student
 console.log(nom, skills, scores.slice(2).toString());
+
+
+// Reponse numero 3
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+  ]
+function convertArrayToObject(params) {
+    for (const [name, skills, scores] of params) {
+       console.log([{name, skills, scores}])
+    }
+}
+convertArrayToObject(students);
