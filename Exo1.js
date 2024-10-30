f6bb0075ec0c0e2c5feca537feed37024bbbd84e
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-30 12:49:26 +0100

    Question 1 terminer:Déstructurez et attribuez les éléments du tableau de constantes à e, pi, gravity, humanBodyTemp, waterBoilingTemp.

---
+++ Exo1.js
@@ -0,0 +1,4 @@
+// Reponse numero 1
+const constants = [2.72, 3.14, 9.81, 37, 100]
+let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants
+console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp);
