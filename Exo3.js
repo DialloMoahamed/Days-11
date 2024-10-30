f9e22b2e3eb5ba8d06d160fd47606f11b70c02b0
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-30 14:40:24 +0100

    Question 2 terminer:Un développeur junior structure le nom, les compétences et le score de l'étudiant dans un tableau de tableaux qui peut ne pas être facile à lire. Déstructurez le tableau suivant en nom, le tableau de compétences en compétences, le tableau de scores en scores, le score JavaScript en jsScore et le score React en variable reactScore sur une seule ligne.

--- Exo3.js
+++ Exo3.js
@@ -1,3 +1,10 @@
+// Reponse numero 1
 for (const {name, capital, population, languages} of countries) {
     console.log(name, capital, population, languages);
 }
+
+
+// Reponse numero 2
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+let [nom, skills, scores] = student
+console.log(nom, skills, scores.slice(2).toString());
