4008f0551777ca224378a3bcb4d0def58114e8a5
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-30 12:52:13 +0100

    Question 2 terminer:Déstructurer et affecter les éléments du tableau des pays à fin, est, sw, den, nor

--- Exo1.js
+++ Exo1.js
@@ -2,3 +2,9 @@
 const constants = [2.72, 3.14, 9.81, 37, 100]
 let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants
 console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp);
+
+
+// Reponse numero 2
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+let [fin, est, sw, den, nor] = countries
+console.log(fin, est, sw, den, nor);
