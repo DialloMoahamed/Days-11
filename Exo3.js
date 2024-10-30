991581b3e0804486453c64909a27433f15915ef7
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-30 15:33:49 +0100

    Question 4 terminer:Copiez l'objet étudiant dans newStudent sans muter l'objet d'origine. Dans le nouvel objet, ajoutez ce qui suit ? Ajoutez Bootstrap avec le niveau 8 aux ensembles de compétences du front-end Ajoutez Express avec le niveau 9 aux ensembles de compétences back-end Ajoutez SQL avec le niveau 8 aux compétences de la base de données Ajoutez SQL sans niveau aux compétences en science des données

--- Exo3.js
+++ Exo3.js
@@ -21,3 +21,32 @@ function convertArrayToObject(params) {
     }
 }
 convertArrayToObject(students);
+
+
+// Reponse numero 4
+const étudiant = {
+    name: 'David',
+    age: 25,
+    skills: {
+      frontEnd: [
+        { skill: 'HTML', level: 10 },
+        { skill: 'CSS', level: 8 },
+        { skill: 'JS', level: 8 },
+        { skill: 'React', level: 9 }
+      ],
+      backEnd: [
+        { skill: 'Node',level: 7 },
+        { skill: 'GraphQL', level: 8 },
+      ],
+      dataBase:[
+        { skill: 'MongoDB', level: 7.5 },
+      ],
+      dataScience:['Python', 'R', 'D3.js']
+    }
+  }
+let newEtudiant = {...étudiant}
+newEtudiant.skills.frontEnd.push({skill: 'Bootstrap', level: 8})
+newEtudiant.skills.backEnd.push({skill: 'Express', level: 9})
+newEtudiant.skills.dataBase.push({skill: 'SQL', level: 8})
+newEtudiant.skills.dataScience.push({skill: 'SQL'})
+console.log(newEtudiant);
